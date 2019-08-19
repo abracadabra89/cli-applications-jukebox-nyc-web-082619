eea4c4a1822200d38c6670229c7e6b981964a10c
anya litvinova <anya.s.litvinova@gmail.com>
2019-08-19 17:25:49 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -55,8 +55,27 @@ end
 
 
 
-def run  
- puts "Please enter a command:"
-  gets.chomp()
+def run(songs)
+  help()
+  input = prompt()
+  
+  while input != "exit"
+    if input == "list"
+      list(songs)
+      input = prompt()
+    elsif input == "play"
+      play(songs)
+      input = prompt()
+    elsif input == "help"
+      help()
+      input = prompt()
+    else
+      puts "Invalid command"
+      help()
+      input = prompt()
+    end
+  end
+  
+  exit_jukebox()
 end
 
